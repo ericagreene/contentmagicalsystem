fdaf3bbbfb604e026cf4797227ce92879b747d83
Sean Creeley <sean.creeley@gmail.com>
2015-07-12 14:45:35 -0400

    Fix

--- web/js/modules/test.js
+++ web/js/modules/test.js
@@ -69,8 +69,81 @@ module.exports = {
           "who": "Erica",
           "start_state": 2,
           "end_state": 3
+        },
+         {
+          "timestamp": "2015-07-11T23:39:40+00:00",
+          "who": "Sara",
+          "start_state": 3,
+          "end_state": 4
+        }
+      ]
+    },
+    {
+      "id": 3,
+      "slug": "Snowden08",
+      "actions": [
+        {
+          "timestamp": "2015-07-11T23:38:47+00:00",
+          "who": "Sara",
+          "start_state": 1,
+          "end_state": 2
+        },
+         {
+          "timestamp": "2015-07-11T23:49:47+00:00",
+          "who": "Sean",
+          "start_state": 2,
+          "end_state": 3
+        },
+        {
+          "timestamp": "2015-07-11T23:53:47+00:00",
+          "who": "Wendy",
+          "start_state": 3,
+          "end_state": 4
+        },
+        {
+          "timestamp": "2015-07-11T23:53:47+00:00",
+          "who": "Sara",
+          "start_state": 4,
+          "end_state": 1
+        }
+      ]
+    },
+    {
+      "id": 4,
+      "slug": "Greece08",
+      "actions": [
+        {
+          "timestamp": "2015-07-11T21:50:47+00:00",
+          "who": "Ashley",
+          "start_state": 1,
+          "end_state": 2
+        },
+         {
+          "timestamp": "2015-07-11T22:19:47+00:00",
+          "who": "Sara",
+          "start_state": 2,
+          "end_state": 1
+        },
+        {
+          "timestamp": "2015-07-11T22:30:47+00:00",
+          "who": "Ashley",
+          "start_state": 1,
+          "end_state": 2
+        },
+        {
+          "timestamp": "2015-07-11T22:35:47+00:00",
+          "who": "Sara",
+          "start_state": 2,
+          "end_state": 3
+        },
+         {
+          "timestamp": "2015-07-11T22:39:40+00:00",
+          "who": "Wendy",
+          "start_state": 3,
+          "end_state": 4
         }
       ]
     }
   ]
-};
+}
+
