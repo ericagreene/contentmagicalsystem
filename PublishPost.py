418885a3d9d4d5cf4456a369fdeecae15834ccdb
Giridhar Gomatom <giridhar.passion@gmail.com>
2015-07-11 19:18:26 -0700

    Update Publish Post method
    Change to have one function with title and body arguments

--- PublishPost.py
+++ PublishPost.py
@@ -1,6 +1,5 @@
 __author__ = 'Thinker'
-import json
-from pprint import pprint
+
 from wordpress_xmlrpc import Client, WordPressPost
 from wordpress_xmlrpc.methods.posts import GetPosts, NewPost
 
@@ -11,38 +10,34 @@ def setup(url,username,token):
 def Fetch_Posts(self):
     # Connect to Word Press API
     wp=setup('https://hackingjournalismtest.wordpress.com/xmlrpc.php', 'contentmagicalsystem','aA9&cG^%wqSkd7MxHU@PYT72c&h')
-    print(wp.call(GetPosts()))
+    return wp.call(GetPosts())
 
-def Publish_Post(wp,request):
+def Publish_Post(title,body):
     # Connect to Word Press POST API
+    obj=setup('https://hackingjournalismtest.wordpress.com/xmlrpc.php', 'contentmagicalsystem','aA9&cG^%wqSkd7MxHU@PYT72c&h')
     post = WordPressPost()
 
-    json_data = json.loads(request)
-    # Title , Content mandatory fields
-    post.title = request['summary']
-    post.content = request['body']
-    '''
-    # Render the images
-    for photo in request['photo']:
-        post.content += "<img>"+photo+"</img>"
+    if len(title) == 0 :
+        raise("Cant Process the request")
+        return "Empty title requested"
 
-    post.title = 'New line Test Paragraph Test New 2'
-    post.content="<p>Hello and welcome to The Post.<p>" \
-                     "<p>We wanted to provide <br/> some of our data for you to hack on.<p>"
-
+    if len(body) == 0:
+        rasie("Cant Process the request")
+        return "Empty body requested"
     '''
+    Future or Next in line
+    Better data validations
+    Have some other quality checks for non ascii charecters and valdiate unicode letters if required
+    Request type validation (old vs new)
+    check if title already exist and update postif required
+    '''
+
+    post.title=title
+    post.content=body
     # Make post visible ,status should be  publish
     post.post_status = 'publish'
-    if (len(post.title) == 0 and len(post.content) == 0):
-        raise("Cant Process the request")
-        return "Invalid JSON object requested"
 
     # API call to push it to word press
     post.id=wp.call(NewPost(post))
-    return "Post created with id ",post.id
-
-# Some test
-jsonresp=[""]
-obj=setup('https://hackingjournalismtest.wordpress.com/xmlrpc.php', 'contentmagicalsystem','aA9&cG^%wqSkd7MxHU@PYT72c&h')
-res=Publish_Post(obj,jsonresp)
-print(res)
+    #return "Post created with id ",post.id
+    return post.id
